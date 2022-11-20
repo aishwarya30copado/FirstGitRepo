f10a2d6d79d6437460fcd20f08347e2e4d6c50d7
Aishwarya <64596077+aishwarya30copado@users.noreply.github.com>
2022-11-20 16:04:57 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,79 @@
+//console.log('hello');
+//alert('yohhhhhh');
+
+var a = 'sommthiee';
+//alert(a);
+//console.log(a);
+
+//document.getElementById('oneid').innerHTML = 'Aishwarya';
+
+//var age = prompt('what\'s your age?');
+//document.getElementById('oneid').innerHTML = age;
+
+/*Functions
+1. cretate a function with logic
+2. call that function
+*/
+function fun(){
+    alert('entered function');
+}
+//callingfunction
+//fun();
+
+/*
+function to get name and greet him/her
+*/
+function greeting(){
+    var name = prompt('what\'s your name?');
+    var result = 'Hello' + ' ' + name;
+    console.log(result);
+}
+//greeting();
+
+/*
+functions taking in arguments
+*/
+function name1(args){
+    var result = 'hello' + args;
+    console.log(result);
+    alert(result);
+}
+//var name = prompt('what\'s your name?');
+//name1(name);
+
+/*loops - if things have to be done multiple times
+*/
+var num1 = 0;
+
+/*while (num1 < 50){
+    num1+=1;
+    console.log(num);
+
+let num;
+for(num=0; num<=100; num++)
+{
+    console.log(num);
+}
+}*/
+
+//datatypes--integers and strings
+let yourAge = 24;//number
+let yourName = 'aishu';//string
+let name = {first:'aishu', last: 'pen'};//object
+let truth = false;//boolean
+let groceries = ['apple','banana','orange'];//array
+
+
+//strings and commomn methods, index number, slicing scring,new line, escape character
+let fruit = 'kiwi';
+
+//Array
+
+
+
+
+
+
+
+
+
